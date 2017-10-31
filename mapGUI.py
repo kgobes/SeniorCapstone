3f87c2dcc41140571bb868951eeda4ea7ca5b064
christiealtadonna <cha415@lehigh.edu>
2017-10-31 12:09:20 -0400

    Minor changes to mapGUI and story_telling to get Lili to start based on clicking play

--- mapGUI.py
+++ mapGUI.py
@@ -8,6 +8,21 @@ from PyQt4.QtGui import *
 #from story_telling import test
 from dict_map import dictMap
 import os
+#from story_telling import test
+
+#imports for test() from story_telling
+import time
+import rospy
+import test_story
+import cinema_story
+import pickle
+from dialog import Dialog
+from player import Player
+from story import Story
+from lili_avatar import LILIAvatar
+
+
+
 
 #app is main application- our platform for interaction with the user
 app = QApplication(sys.argv)
@@ -286,7 +301,7 @@ def chooseActivitiesHelper():
 		print(activitySelect.currentText())
 		#Saving the choices for scenes
 		#TODO - add the scene from the dictMap scene -may need to implement a hasmap for scene
-		mymap.locations[locationChosen].list_of_scenes.append()
+		#mymap.locations[locationChosen].list_of_scenes.append()
 
 		global sceneNum
 		sceneNum.setNum(numscene)
@@ -330,7 +345,7 @@ def mainMenu():
 #	playExisting.clicked.connect(exit) #eventually will have list of stories and then play them
 
 	#UNCOMMENT WHEN USING STORY_TELLING.PY AGAIN
-#	playExisting.clicked.connect(test) #eventually will have list of stories and then play them
+	playExisting.clicked.connect(test) #eventually will have list of stories and then play them
 	playExisting.move(375, 250)
 	#button for speech recognition 
 	addPlayers = QPushButton("Add Players", startScreen)
@@ -343,5 +358,44 @@ def mainMenu():
 #main()
 
 
+
+
+
+def test():
+	#rospy.init_node('story_telling')
+	avatar = LILIAvatar()
+	time.sleep(1)
+
+	avatar.speak('Hi! My name is Lili. What is yours?')
+	name = avatar.listen()
+
+	#add Dictionary Map creation
+	#dict_map = StoryMap("Predefined Map")
+
+	#player = Player(test_story.zoo_start, test_story.zoo_nodes, name)
+	player = Player(cinema_story.cinema_start, cinema_story.cinema_nodes, name)
+	
+	#avatar.speak('Hi ' + name + ', nice to meet you.')
+
+	#======== DYNAMIC FUNCTIONALITY to be implemented ====
+	#zoo_story = Story(player, test_story.zoo_start)
+	movie_story = Story(player, cinema_story.cinema_start)
+
+    
+	f = open('pickleTest', 'w')
+	#pickle.dump(zoo_story, f)
+	pickle.dump(movie_story, f)
+	f.close()
+
+	f = open('pickleTest', 'r')
+	newStory = pickle.load(f)
+	f.close()
+
+	newStory.walk(player, avatar)
+	mapGUI.main()
+
+
+
+
 #if __name__ == '__main__':
-main()
+#main()
